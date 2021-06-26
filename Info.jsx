a65d94bca8ea4da42d85009a9876dab52b0d4280
JE5WIN <77658387+JE5WIN@users.noreply.github.com>
2021-06-26 18:31:15 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div class ="note">
+    <h1>Javascript and React.js</h1>
+    <p> A basic web development React Js Bootcamp </p>
+    </div>
+  );
+}
+
+export default Info;
